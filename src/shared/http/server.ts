94180c986d99e9fd21a684466c9b58ab81dac440
Aluizio Developer <aluiziodeveloper@gmail.com>
2020-12-08 11:22:15 -0300

    Config migrations

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
