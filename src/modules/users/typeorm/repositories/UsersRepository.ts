642d622bbea49bdda351d4492174a322ef186907
Aluizio Developer <aluiziodeveloper@gmail.com>
2020-12-09 15:15:20 -0300

    Users Repository

---
+++ src/modules/users/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,37 @@
+import { EntityRepository, Repository } from 'typeorm';
+import User from '../entities/User';
+
+@EntityRepository(User)
+class UsersRepository extends Repository<User> {
+  public async findByName(name: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        name,
+      },
+    });
+
+    return user;
+  }
+
+  public async findById(id: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        id,
+      },
+    });
+
+    return user;
+  }
+
+  public async findByEmail(email: string): Promise<User | undefined> {
+    const user = await this.findOne({
+      where: {
+        email,
+      },
+    });
+
+    return user;
+  }
+}
+
+export default UsersRepository;
