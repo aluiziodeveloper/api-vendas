47d44e11b75975f82cb713230e1af57c8d9584ab
Aluizio Developer <aluiziodeveloper@gmail.com>
2020-12-14 01:00:03 -0300

    Entidade UserTokens

---
+++ src/modules/users/typeorm/entities/UserToken.ts
@@ -0,0 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  Generated,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class UserToken {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  @Generated('uuid')
+  token: string;
+
+  @Column()
+  user_id: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default UserToken;
