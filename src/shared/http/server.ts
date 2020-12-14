022a3d5c5c633421545d0e1170ed50188a41e8ad
Aluizio Developer <aluiziodeveloper@gmail.com>
2020-12-14 00:39:46 -0300

    Static route

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -6,12 +6,13 @@ import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
+import uploadConfig from '@config/upload';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-
+app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
 
 app.use(errors());
