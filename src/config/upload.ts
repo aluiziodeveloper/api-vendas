cb1950ef1beea2455ea7c84719e51da92a53ca4a
Aluizio Developer <aluiziodeveloper@gmail.com>
2020-12-13 23:26:50 -0300

    Multer (upload)

---
+++ src/config/upload.ts
@@ -0,0 +1,19 @@
+import path from 'path';
+import multer from 'multer';
+import crypto from 'crypto';
+
+const uploadFolder = path.resolve(__dirname, '..', '..', 'uploads');
+
+export default {
+  directory: uploadFolder,
+  storage: multer.diskStorage({
+    destination: uploadFolder,
+    filename(request, file, callback) {
+      const fileHash = crypto.randomBytes(10).toString('hex');
+
+      const filename = `${fileHash}-${file.originalname}`;
+
+      callback(null, filename);
+    },
+  }),
+};
--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,7 +3,7 @@ import { verify } from 'jsonwebtoken';
 import AppError from '@shared/errors/AppError';
 import authConfig from '@config/auth';
 
-interface TokenPayload {
+interface ITokenPayload {
   iat: number;
   exp: number;
   sub: string;
@@ -25,7 +25,7 @@ export default function isAuthenticated(
   try {
     const decodedToken = verify(token, authConfig.jwt.secret);
 
-    const { sub } = decodedToken as TokenPayload;
+    const { sub } = decodedToken as ITokenPayload;
 
     request.user = {
       id: sub,
