c7e1d202057f43a4fce825e2522a5460c34d4883
Aluizio Developer <aluiziodeveloper@gmail.com>
2020-12-14 01:11:13 -0300

    Repositorio UserTokens

---
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -0,0 +1,27 @@
+import { EntityRepository, Repository } from 'typeorm';
+import UserToken from '../entities/UserToken';
+
+@EntityRepository(UserToken)
+class UserTokensRepository extends Repository<UserToken> {
+  public async findByToken(token: string): Promise<UserToken | undefined> {
+    const userToken = await this.findOne({
+      where: {
+        token,
+      },
+    });
+
+    return userToken;
+  }
+
+  public async generate(user_id: string): Promise<UserToken | undefined> {
+    const userToken = await this.create({
+      user_id,
+    });
+
+    await this.save(userToken);
+
+    return userToken;
+  }
+}
+
+export default UserTokensRepository;
