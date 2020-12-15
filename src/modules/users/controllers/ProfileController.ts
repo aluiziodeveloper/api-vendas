f7b74c3e0ed2288bef7b0d5d0102315b1a6016d0
Aluizio Developer <aluiziodeveloper@gmail.com>
2020-12-15 15:25:33 -0300

    Controller Profile

---
+++ src/modules/users/controllers/ProfileController.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import ShowProfileService from '../services/ShowProfileService';
+import UpdateProfileService from '../services/UpdateProfileService';
+
+export default class ProfileController {
+  public async show(request: Request, response: Response): Promise<Response> {
+    const showProfile = new ShowProfileService();
+    const user_id = request.user.id;
+
+    const user = await showProfile.execute({ user_id });
+
+    return response.json(user);
+  }
+
+  public async update(request: Request, response: Response): Promise<Response> {
+    const user_id = request.user.id;
+    const { name, email, password, old_password } = request.body;
+
+    const updateProfile = new UpdateProfileService();
+
+    const user = await updateProfile.execute({
+      user_id,
+      name,
+      email,
+      password,
+      old_password,
+    });
+
+    return response.json(user);
+  }
+}
