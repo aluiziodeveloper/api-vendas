e1d77a52bb92221f3be569bee31a0d0e85beeb2a
Aluizio Developer <aluiziodeveloper@gmail.com>
2020-12-15 13:50:19 -0300

    Mail Template Handlebars - test

--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -19,13 +19,23 @@ class SendForgotPasswordEmailService {
       throw new AppError('User does not exists.');
     }
 
-    const token = await userTokensRepository.generate(user.id);
+    const { token } = await userTokensRepository.generate(user.id);
 
     //console.log(token);
 
     await EtherealMail.sendMail({
-      to: email,
-      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[API Vendas] Recuperação de Senha',
+      templateData: {
+        template: `Olá {{name}}: {{token}}`,
+        variables: {
+          name: user.name,
+          token,
+        },
+      },
     });
   }
 }
--- src/modules/users/typeorm/repositories/UserTokensRepository.ts
+++ src/modules/users/typeorm/repositories/UserTokensRepository.ts
@@ -13,7 +13,7 @@ class UserTokensRepository extends Repository<UserToken> {
     return userToken;
   }
 
-  public async generate(user_id: string): Promise<UserToken | undefined> {
+  public async generate(user_id: string): Promise<UserToken> {
     const userToken = await this.create({
       user_id,
     });
