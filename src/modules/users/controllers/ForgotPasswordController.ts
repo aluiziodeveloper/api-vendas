fb781a15b6c22dda2042cbd8c6e14cf6e5ca38b7
Aluizio Developer <aluiziodeveloper@gmail.com>
2020-12-15 10:42:47 -0300

    Forgot Password Controller

---
+++ src/modules/users/controllers/ForgotPasswordController.ts
@@ -0,0 +1,16 @@
+import { Request, Response } from 'express';
+import SendForgotPasswordEmailService from '../services/SendForgotPasswordEmailService';
+
+export default class ForgotPasswordController {
+  public async create(request: Request, response: Response): Promise<Response> {
+    const { email } = request.body;
+
+    const sendForgotPasswordEmail = new SendForgotPasswordEmailService();
+
+    await sendForgotPasswordEmail.execute({
+      email,
+    });
+
+    return response.status(204).json();
+  }
+}
