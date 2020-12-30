eea0afc1522e8437e4708e48900b967d327fc2cd
Aluizio Developer <aluiziodeveloper@gmail.com>
2020-12-29 22:57:30 -0300

    Show Order Service

---
+++ src/modules/orders/services/ShowOrderService.ts
@@ -0,0 +1,24 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import Order from '../typeorm/entities/Order';
+import OrdersRepository from '../typeorm/repositories/OrdersRepository';
+
+interface IRequest {
+  id: string;
+}
+
+class ShowOrderService {
+  public async execute({ id }: IRequest): Promise<Order> {
+    const ordersRepository = getCustomRepository(OrdersRepository);
+
+    const order = await ordersRepository.findById(id);
+
+    if (!order) {
+      throw new AppError('Order not found.');
+    }
+
+    return order;
+  }
+}
+
+export default ShowOrderService;
