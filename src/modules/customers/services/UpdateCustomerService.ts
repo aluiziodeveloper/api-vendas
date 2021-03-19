e0475751713de837037c1bfdca097ba83e876462
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-03-18 21:21:14 -0300

    Refactoring UpdateCustomerService

--- src/modules/customers/services/UpdateCustomerService.ts
+++ src/modules/customers/services/UpdateCustomerService.ts
@@ -1,6 +1,6 @@
 import AppError from '@shared/errors/AppError';
-import { compare, hash } from 'bcryptjs';
 import { getCustomRepository } from 'typeorm';
+import Customer from '../typeorm/entities/Customer';
 import CustomersRepository from '../typeorm/repositories/CustomersRepository';
 
 interface IRequest {
@@ -10,7 +10,7 @@ interface IRequest {
 }
 
 class UpdateCustomerService {
-  public async execute({ id, name, email }: IRequest): Promise<User> {
+  public async execute({ id, name, email }: IRequest): Promise<Customer> {
     const customersRepository = getCustomRepository(CustomersRepository);
 
     const customer = await customersRepository.findById(id);
