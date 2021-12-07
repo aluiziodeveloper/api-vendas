ed1e4d1f07244d92ffa3a7821d8aa1e3fba8b57c
Aluizio Developer <aluiziodeveloper@gmail.com>
2021-12-07 10:25:20 -0300

    Incluindo a extensao UUID nas migracoes

--- src/shared/typeorm/migrations/1607534203339-CreateUsers.ts
+++ src/shared/typeorm/migrations/1607534203339-CreateUsers.ts
@@ -2,6 +2,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateUsers1607534203339 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'users',
--- src/shared/typeorm/migrations/1609038202583-CreateOrdersProducts.ts
+++ src/shared/typeorm/migrations/1609038202583-CreateOrdersProducts.ts
@@ -2,6 +2,7 @@ import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CreateOrdersProducts1609038202583 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"');
     await queryRunner.createTable(
       new Table({
         name: 'orders_products',
